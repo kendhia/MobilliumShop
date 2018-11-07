c963a689a1a2ddcbd01de0ec9ae9005f697eca27
Dhia Kennouche <kendhia@gmail.com>
2018-11-08 01:40:19 +0300

    login_activity_ui

--- app/src/main/java/kendhia/me/projects/mobilliumshop/LoginActivity.kt
+++ app/src/main/java/kendhia/me/projects/mobilliumshop/LoginActivity.kt
@@ -1,9 +1,10 @@
 package kendhia.me.projects.mobilliumshop
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
